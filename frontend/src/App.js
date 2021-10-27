9e985f35dbf47ce1984bc6f9177c5878abe1a0bd
Anirudh Rowjee <ani.rowjee@gmail.com>
2021-10-27 08:47:27 +0530

    nuked tailwind and the existing app, restarting with mui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p class="text-large-400 text-blue-500">Hello, world!</p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
